76dd81577a2966fe8f19ae04495ad43cc36b8142
Clinton-Rodrigues <clintonrodrigues.dev@gmail.com>
2023-06-20 20:58:00 -0300

    Upadte

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,16 +1,12 @@
-const logger = require('../../logger');
+const logger = require('../logger');
 
 function errorHandler(err, req, res, next) {
-    console.error(err); // Exibe o erro no console para fins de depuração
-    if (!res.headersSent) {
-        res.status(500).json({ message: 'Erro interno do servidor' });
-      }
-    logger.error('Erro capturado', { error: err });
-    const statusCode = err.status || 500;
-    const message = err.message || 'Erro interno do servidor';
-  
-    res.status(statusCode).json({ message });
-  }
-  
-  module.exports = errorHandler;
-  
+  console.error(err); // Exibe o erro no console para fins de depuração
+  logger.error('Erro capturado', { error: err });
+  const statusCode = err.status || 500;
+  const message = err.message || 'Erro interno do servidor';
+
+  res.status(statusCode).json({ message });
+}
+
+module.exports = errorHandler;
