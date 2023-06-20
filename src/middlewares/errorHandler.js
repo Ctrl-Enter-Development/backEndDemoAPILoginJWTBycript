bd5382d03709af22e048f33973f8098f9dffd488
Clintondev <clintonrodrigues.dev@gmail.com>
2023-06-20 15:18:09 -0300

    Update routes

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,6 +2,9 @@ const logger = require('../../logger');
 
 function errorHandler(err, req, res, next) {
     console.error(err); // Exibe o erro no console para fins de depuração
+    if (!res.headersSent) {
+        res.status(500).json({ message: 'Erro interno do servidor' });
+      }
     logger.error('Erro capturado', { error: err });
     const statusCode = err.status || 500;
     const message = err.message || 'Erro interno do servidor';
