6b5589b6b856dfe4b2c9777348459de94463a1ea
Clinton-Rodrigues <clintonrodrigues.dev@gmail.com>
2023-06-20 21:02:45 -0300

    Upadte

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,4 +1,4 @@
-const logger = require('../logger');
+const logger = require('../../logger');
 
 function errorHandler(err, req, res, next) {
   console.error(err); // Exibe o erro no console para fins de depuração
